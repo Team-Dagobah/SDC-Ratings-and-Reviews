e66244e156cf107aa3cbbfd9a8253bcc73517fdd
Lawrence Sanzogni <lawrencesanzogni@gmail.com>
2022-08-03 17:21:36 -0500

    k6 code pushed up

---
+++ test.js
@@ -0,0 +1,32 @@
+import http from 'k6/http';
+import {sleep, check} from 'k6';
+import {Counter} from 'k6/metrics';
+
+export const requests = new Counter('http_reqs');
+
+const end = Math.ceil(5774952);
+const pivot = Math.floor(2000000);
+const product_id = Math.floor(Math.random() * (end - pivot) + pivot);
+const url = `http://localhost:3000/reviews/meta/?product_id=${product_id}`;
+
+export const options = {
+  stages: [
+    {duration: '10s', target: 10},
+    {duration: '10s', target: 20},
+    {duration: '10s', target: 40},
+    {duration: '10s', target: 80},
+    {duration: '10s', target: 160}
+  ]
+}
+
+export default function () {
+ const res = http.get(url);
+  sleep(1);
+  check(res, {
+    'is status 200': r => r.status === 200,
+    'transaction time < 200ms': r => r.timings.duration < 200,
+    'transaction time < 500ms': r => r.timings.duration < 500,
+    'transaction time < 1000ms': r => r.timings.duration < 1000,
+    'transaction time < 2000ms': r => r.timings.duration < 2000,
+  });
+}
